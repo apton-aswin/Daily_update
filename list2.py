69e448be771bb6524d01591aef08550c537ff4e0
aswin-apton <aswin@aptonworks.com>
2022-02-24 13:50:17 +0530

    daily update

--- list2.py
+++ list2.py
@@ -13,8 +13,8 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-    list = list(dict.fromkeys(list))
-    return list
+    nums = list(dict.fromkeys(nums))
+    return nums
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
